3fa26e603b7b654f3d525271edf5f7d097dfbcf2
eitan <eitan.vichik@gmail.com>
2023-11-20 15:22:11 +0200

    Fix argument handling in main.c

--- main.c
+++ main.c
@@ -2,14 +2,13 @@
 #include "AsciiArtTool.h"
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Invalid arguments!");
+    if (argc != 4) {
         return 0;
     }
 
-    char* flag = argv[0];
-    char* source_path = argv[1];
-    char* destination_path = argv[2];
+    char* flag = argv[1];
+    char* source_path = argv[2];
+    char* destination_path = argv[3];
 
     FILE* source_file = fopen(source_path, "r");
     if (source_file == NULL) {
