214c4d4a78198ae0684c65d93ec99627dfa895ed
eitan <eitan.vichik@gmail.com>
2023-11-20 15:09:29 +0200

    Change cmake version to 3.26.4

---
+++ cmake-build-debug/CMakeFiles/MATAM2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MATAM2.dir/AsciiArtTool.c.obj"
+  "CMakeFiles/MATAM2.dir/AsciiArtTool.c.obj.d"
+  "CMakeFiles/MATAM2.dir/RLEList.c.obj"
+  "CMakeFiles/MATAM2.dir/RLEList.c.obj.d"
+  "MATAM2.exe"
+  "MATAM2.exe.manifest"
+  "MATAM2.pdb"
+  "libMATAM2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MATAM2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/MATAM2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MATAM2.
+# This may be replaced when dependencies are built.
